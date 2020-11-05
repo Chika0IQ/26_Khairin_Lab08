b27ec3eb5729e1c5b9a73f10ce2055576b924cc3
Chika0IQ <mohdkhairin62@gmail.com>
2020-11-05 14:28:12 +0800

    Initial commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    float spacepressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreText.GetComponent<Text>().text = "SpacePress:" + spacepressed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spacepressed++;
+            print(spacepressed);
+            scoreText.GetComponent<Text>().text = "SpacePress:" + spacepressed;
+        }
+    }
+}
